dc6571ce5d387bbd053baa7297731aabcac378d4
adrianomazucato <amazucato@gmail.com>
2022-12-31 10:19:42 -0300

    fix

--- PhotoFaceSample/Scenes/Login/LoginViewModel.swift
+++ PhotoFaceSample/Scenes/Login/LoginViewModel.swift
@@ -219,7 +219,7 @@ extension LoginViewModel {
     }
       
       //Tira a tela quando submeter as imagens
-      viewController?.navigationController?.popViewController(animated: true)
+      viewController?.navigationController?.popToRootViewController(animated: false)
   }
   
     func createSession(onComplete: @escaping ()->Void) {
